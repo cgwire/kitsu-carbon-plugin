f071006ce78e61d277727a59cb6939eb51a3dc9b
Nicolas Ledez <247138+nledez@users.noreply.github.com>
2026-04-07 15:15:00 +0200

    ci: add GitHub Actions release workflow and adapt release script
      - Add workflow that builds frontend and creates a zip on tag push
      - Adapt release.sh to read/bump version from manifest.toml

---
+++ scripts/release.sh
@@ -0,0 +1,8 @@
+set -e
+git pull --rebase origin main
+last_release_number=$(grep '^version' manifest.toml | sed 's/version = "\(.*\)"/\1/')
+release_number=$(echo ${last_release_number} | awk -F. -v OFS=. '{$NF += 1 ; print}')
+sed -i'' -e "s/version = \"$last_release_number\"/version = \"$release_number\"/" manifest.toml
+git commit manifest.toml -m $release_number
+git tag -a -m "Release v$release_number" v$release_number
+git push origin main --follow-tags
